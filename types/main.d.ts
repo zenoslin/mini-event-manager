ac2859c74965c36c73f204f9c2e7d7ac261ee4e0
zenoslin <zenoslin@outlook.com>
2019-10-16 12:01:58 +0800

    feat: update type file

--- types/main.d.ts
+++ types/main.d.ts
@@ -1,2 +1,15 @@
-declare const _default: () => string;
-export default _default;
+declare type Listener = ((...payload: any) => void) & {
+    isOnce?: boolean;
+};
+export default class MiniEvent {
+    private _listenersMap;
+    constructor();
+    on(evnetName: string, linstener: Listener): MiniEvent;
+    once(evnetName: string, listener: Listener): MiniEvent;
+    off(evnetName: string, linstener?: Listener): MiniEvent;
+    emit(evnetName: string, ...payload: any): boolean;
+    has(eventName: string): boolean;
+    eventNames(): string[];
+    destory(): void;
+}
+export {};
